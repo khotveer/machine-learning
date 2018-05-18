8837eab5028d3349f4ae01f4a3c48b9d695a8a2e
veerdhaval khot <khotveer1@gmail.com>
2018-05-18 09:21:13 +0530

    Add files via upload

---
+++ SVM/svm_classififcation_example1.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 17 16:43:54 2018
+
+@author: khot
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as  plt
+
+#importing dataset
+ds = pd.read_csv("Social_Network_Ads.csv")
+ds.shape
+#checking for null valiuse
+ds.isnull().sum().sum()
+#checking data types of each features
+ds.dtypes
+
+x = ds.iloc[:,[2,3]].values
+y = ds.iloc[:,4].values
+
+#splitting data 
+from sklearn.cross_validation import train_test_split
+x_train , x_test , y_train , y_test = train_test_split(x , y , test_size=0.20 ,random_state=0)
+
+#feature scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.fit_transform(x_test)
+
+
+#fitting the classifier
+from sklearn.svm import SVC
+classifier = SVC(kernel='linear' , random_state=0)
+classifier.fit(x_train,y_train)
+#predicting y
+y_predict = classifier.predict(x_test)
+
+#confusion matrix
+from sklearn.metrics import confusion_matrix
+print("confusion metrics : \n", confusion_matrix(y_test , y_predict))
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = x_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVM (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = x_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVM (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
